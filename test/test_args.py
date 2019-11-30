88a6351c4b0492d133b5b629fe4f7ab1b642522f
Gianluca Rossi <gr.gianlucarossi@gmail.com>
2019-11-30 11:01:24 -0500

    DEV: remove args in editable mode from requirements.txt

--- test/test_args.py
+++ test/test_args.py
@@ -7,10 +7,7 @@ from args.args import Args
     "pattern,in_,expected", [("l", ["True"], True), ("l", ["False"], False),]
 )
 def test_get_bool(pattern, in_, expected):
-    # when
     arg = Args(pattern, in_)
-
-    # then
     assert arg.get_bool("l") == expected
 
 
@@ -18,10 +15,7 @@ def test_get_bool(pattern, in_, expected):
     "pattern,in_,expected", [("p#", ["99999"], 99999), ("p#", ["0"], 0),]
 )
 def test_get_int(pattern, in_, expected):
-    # when
     arg = Args(pattern, in_)
-
-    # then
     assert arg.get_int("p") == expected
 
 
@@ -30,8 +24,5 @@ def test_get_int(pattern, in_, expected):
     [("a*.", ["99999"], "99999"), ("a*.", ["True"], "True"),],
 )
 def test_get_str(pattern, in_, expected):
-    # when
     arg = Args(pattern, in_)
-
-    # then
     assert arg.get_str("a") == expected
